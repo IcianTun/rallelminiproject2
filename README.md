6615d3da6b0cf817cbbce14ee343b825bdee550a
eieieician <magician_TH@hotmail.com>
2019-04-17 21:44:24 +0700

    initial

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # rallelminiproject2
 Miniproject2 2561: LINE-like app management server (10%)
+
+go read dogc
+https://www.mycourseville.com/sites/all/modules/courseville/files/uploads/2018_2/2110315/materials/miniproject2.333.1555476850.1127.pdf
---
+++ server.js
@@ -0,0 +1,110 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+// support parsing of application/json type post data
+app.use(bodyParser.json());
+
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cors());
+
+//---------------
+
+var rooms = [];
+
+//---------------
+
+
+app.get('/', (req, res) => {
+	res.sendStatus(404);
+});
+
+
+app.get('/allrooms',(req,res)=>{
+	res.send(rooms);
+});
+app.post('/allrooms',(req,res)=>{
+	console.log('POST allrooms reqdata')
+	console.log(req.body);
+	var reqdata = req.body;
+	if (!reqdata.id || rooms.includes(reqdata.id)){
+		res.status(404).send("ROOM_ID already exists");
+	} else {
+		rooms.push(reqdata.id);
+		res.status(201).send(reqdata);
+	}
+});
+//----------------------------------------------------------------------
+/*
+app.post('/cart',(req,res)=>{
+	var items = req.body.listOfProduct;
+	//console.log(items)
+	var OrderID = 322;
+	var totalPrice = 0;
+	var insertValuesTxt = '';
+	for (i = 0; i< items.length; i++ ){
+		totalPrice += items[i].Price;
+		insertValuesTxt += '('+items[i].ItemID+', '+OrderID+')';
+		if ( i < items.length -1 ) {
+		insertValuesTxt += ',';
+		}
+	}
+	console.log(insertValuesTxt);
+	console.log('total Price kub '+totalPrice);
+	var datetime = new Date().toISOString().slice(0, 19).replace('T', ' ');
+	var txtkub = "INSERT INTO purchase_order (OrderID, TotalPrice, Orderdate, CouponID) VALUES ("+OrderID+", "+totalPrice+", '"+datetime+"', NULL);";
+	conn.query(txtkub, function(err,results){
+		if(err)console.log(err);//throw err;
+	})
+	conn.query('INSERT INTO SENT_TO (ItemID,OrderID) Values '+insertValuesTxt+';', function(err,response){
+		if(err)console.log(err);//throw err;
+	})
+	
+});
+
+//--------THIS ONE IS OPTIONAL , didn't used :( --------------------------------------------------------------
+app.get('/home', (req,res) => {
+	conn.query("select * from item natural join item_name;", function(err,results){
+		if(err)throw err;
+		console.log(results);
+		res.json(results);
+	})
+});
+
+//----------------------------------------------------------------------
+app.post('/AccPro',(req,res)=>{
+	//console.log(req.body); // { newUserName : ' ASDAfasd' }
+	var newUserNamekub = req.body.newUserName;
+	console.log(newUserNamekub);
+	conn.query("UPDATE user SET Username = '"+newUserNamekub+"' WHERE (Username = '"+oldName+"');",function(err,results){
+		if(err)throw err;
+	});
+	oldName = newUserNamekub;
+})
+
+//----------------------------------------------------------------------
+app.get('/productManage', (req,res) => {
+	conn.query('SELECT ItemID,ItemName,Price FROM ITEM NATURAL JOIN item_Name',function(err,results){
+		if(err) throw err;
+		console.log(results);
+		res.json(results);
+	});
+	
+});
+app.post('/productManage',(req,res)=>{
+	console.log(req.body);
+	var deletedID = req.body.ItemID;
+	conn.query("DELETE item, item_name FROM item INNER JOIN item_name ON item.ItemName = item_name.ItemName WHERE item.ItemID = "+deletedID+";",function(err,results){
+		if(err)throw err;
+	});
+})
+*/
+//----------------------------------------------------------------------
+app.listen(3000,()=>{
+	console.log('app is running on port 3000 kub')
+});
+
